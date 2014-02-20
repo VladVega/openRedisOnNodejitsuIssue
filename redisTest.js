9265a246887f7488b21b2c462dd7649ec035929e
Vlad Vega <vlad@jixee.me>
2014-02-19 16:42:21 -0800

    redis tester

--- redisTest.js
+++ redisTest.js
@@ -2,34 +2,37 @@ var Redis= require('redis');
 var Config = {};
 
 Config.redis= {
-    host: 'node-aaaaa.openredis.com',
+    host: 'host',
     port: 13932,
-    pw: 'f0094bb4467705d32607cc4ac19c727b6f1c2a1458291b8833b61e58e27c3447'
+    pw: 'pw'
 };
 
 process.on('uncaughtException', function(err) {
     console.log('uncaught:',err)
 });
-console.log('start app')
-var publisher = createClient(function(err){
+console.log('App starting');
+
+var client= Redis.createClient(Config.redis.port, Config.redis.host);
+//Redis.debug_mode = true;
+client.auth(Config.redis.pw, function(err){
     if(err){
         console.log('pub auth err:', err);
     }
     console.log('publisher created');
-    publisher.on('error', function(err){
+    client.on('error', function(err){
         console.log('Publisher err:',err);
     });
 
     var testPublish= function(){
-        publisher.publish('test47', 'testhi47');
+        client.publish('test47', 'testhi47');
         console.log('publishtestdone')
     };
 
-    setInterval(testPublish, 3*60*1000);
+    setInterval(testPublish, 20*60*1000);
     testPublish();
-
 });
 
+//open a server so that nodejitsu has an endpoint.
 require('http').createServer(function(req, res) {
     res.end('ok');
 }).listen( process.env.PORT || 5001, function(err){
@@ -40,13 +43,3 @@ require('http').createServer(function(req, res) {
         console.log('created server:'+ (process.env.PORT || 5001));
     });
 
-function createClient(callback){
-    var client= Redis.createClient(Config.redis.port, Config.redis.host);
-    //Redis.debug_mode = true;
-    client.auth(Config.redis.pw, function(err){
-        if(callback){
-            callback(err);
-        }
-    });
-    return client;
-}
